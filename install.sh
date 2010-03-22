88678c564d0c509cf1631e0659455d1dd9063e8e
Rob Hasselbaum <rob@hasselbaum.net>
2010-03-21 20:11:38 -0400

    Misc cleanup prior to first Google Code push.

--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
+#   Copyright (C) 2010 by Rob Hasselbaum <rob@hasselbaum.net>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, see <http://www.gnu.org/licenses/>
 
-# Builds and installs the software using CMake and GNU Make. Call it from
-# within the top-level project directory.
+# This script builds and installs the software using CMake and GNU Make.
+# Call it from within the top-level project directory.
 #
 # This script was adapted from the Fancy Tasks Plasmoid project by Michael D.
 # http://www.kde-look.org/content/show.php?action=content&content=99737.
@@ -43,3 +57,6 @@ then
 fi
 
 kbuildsycoca4
+echo
+echo "You MUST restart Plasma before using the applet."
+echo "Read the INSTALL file for details. Enjoy!"
--- package-ubuntu.sh
+++ package-ubuntu.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
+#   Copyright (C) 2010 by Rob Hasselbaum <rob@hasselbaum.net>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, see <http://www.gnu.org/licenses/>
 
-# Generate (K)ubuntu DEB packages for the current architecture.
-# This probably won't work unless you're running Ubuntu.
+# This script generates (K)ubuntu DEB packages for the current architecture.
+# Call it from the top level project directory. This script probably won't
+# work unless you're running Kubuntu.
 #
 # If you have trouble running this script after a normal build,
 # try clearing out the build directory first. 
 
-# Create an Ubuntu package.
+# This tunction creates a default Ubuntu package for some version of Ubuntu.
+# ARGS:
 # $1: The Debian version (e.g. "karmic1", "lucid1", etc.)
 create_package()
 {
@@ -29,6 +45,7 @@ create_package()
 	cpack
 }
 
+# Main script starts here. Make the build dir.
 if [ ! -d build  ]
 then
     mkdir build
@@ -36,5 +53,6 @@ fi
 
 cd build
 
+# Create Ubuntu packages.
 create_package 'karmic1'
 create_package 'lucid1'
